cb7e283357417eeba025e20c7ef7caa5225f72bf
Juhyeon Jung <juhyeonbt861@naver.com>
2024-03-18 01:38:04 +0900

    mariadb 연동

--- src/main/kotlin/com/example/movie_ticketing/domain/Member.kt
+++ src/main/kotlin/com/example/movie_ticketing/domain/Member.kt
@@ -3,12 +3,12 @@ package com.example.movie_ticketing.domain
 import jakarta.persistence.*
 
 @Entity
-class Member (
-    val email: String? = "",
-    val password: String? = "",
+class Member {
+    val email: String? = null
+    val password: String? = null
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     val id: Long = 0L
-)
+}
