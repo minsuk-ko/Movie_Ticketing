07ffccc90c8b351247c308d71dd36a3459d47f79
Juhyeon Jung <juhyeonbt861@naver.com>
2024-03-24 22:52:02 +0900

    회원가입(임시)

---
+++ src/main/kotlin/com/example/movie_ticketing/service/MemberService.kt
@@ -0,0 +1,30 @@
+package com.example.movie_ticketing.service
+
+import com.example.movie_ticketing.domain.Member
+import com.example.movie_ticketing.repository.MemberRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+class MemberService(var memberRepository : MemberRepository ) {
+
+    /**
+     * 회원가입
+     */
+    fun join(member : Member): Long {
+//        validateDuplicateEmail(member)
+        memberRepository.save(member)
+        return member.id
+    }
+
+    /**
+     * 중복 이메일 방지 (임시)
+     */
+    private fun validateDuplicateEmail(member: Member) {
+        val findEmail = memberRepository.findById(member.id)
+        if (!findEmail.isEmpty){
+            throw IllegalStateException("이미 가입된 이메일입니다 :(")
+        }
+    }
+}
