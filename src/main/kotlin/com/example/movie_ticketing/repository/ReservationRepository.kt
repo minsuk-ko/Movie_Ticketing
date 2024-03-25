48829ea15b96565950fd87d3b976f7874cb3e19d
Juhyeon Jung <juhyeonbt861@naver.com>
2024-03-25 18:26:20 +0900

    reservation repository, service

---
+++ src/main/kotlin/com/example/movie_ticketing/repository/ReservationRepository.kt
@@ -0,0 +1,11 @@
+package com.example.movie_ticketing.repository
+
+import com.example.movie_ticketing.domain.Reservation
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+
+interface ReservationRepository : JpaRepository<Reservation?, Long?> {
+
+    @Query("select m from Reservation m where m.id = :id")
+    fun findOne(id : Long) : Reservation
+}
---
+++ src/main/kotlin/com/example/movie_ticketing/service/ReservationService.kt
@@ -0,0 +1,24 @@
+package com.example.movie_ticketing.service
+
+import com.example.movie_ticketing.domain.Reservation
+import com.example.movie_ticketing.repository.ReservationRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+
+@Service
+@Transactional
+class ReservationService(var reservationRepository: ReservationRepository) {
+
+    fun saveReservation(reservation: Reservation) {
+        reservationRepository.save(reservation)
+    }
+
+    fun findAllReservation() : MutableList<Reservation?> {
+        return reservationRepository.findAll();
+    }
+
+    fun findOne(reservationId : Long): Reservation {
+        return reservationRepository.findOne(reservationId)
+    }
+}
