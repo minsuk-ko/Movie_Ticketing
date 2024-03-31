c1ed8d989d7ec20e0d5116796b8c189862660e7b
Juhyeon Jung <juhyeonbt861@naver.com>
2024-03-31 20:45:26 +0900

    dao

--- src/main/kotlin/com/example/movie_ticketing/domain/Member.kt
+++ src/main/kotlin/com/example/movie_ticketing/domain/Member.kt
@@ -1,10 +1,14 @@
 package com.example.movie_ticketing.domain
 
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import org.springframework.data.annotation.Id
 
 
 data class Member (
-    @Id var id : Int =0,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id : Int =0,
     var name : String ="",
     var email: String ="",
     var age: Int =0,
