4c1c791718794765131e35a5020ff6fc15479b0f
nek0 <juhyeonbt861@naver.com>
2024-05-13 03:46:59 +0900

    movieDetails update

--- src/main/kotlin/com/example/movie_ticketing/dto/MovieDetails.kt
+++ src/main/kotlin/com/example/movie_ticketing/dto/MovieDetails.kt
@@ -1,5 +1,6 @@
 package com.example.movie_ticketing.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.Date
 
@@ -7,16 +8,15 @@ data class Genre(
     @JsonProperty("id") val id: Int,
     @JsonProperty("name") val name: String
 )
-
 data class MovieDetails(
     @JsonProperty("id") val id: Int,
-    @JsonProperty("original_title") val title: String,
+    @JsonProperty("title") val title: String?,
     @JsonProperty("poster_path") val posterPath: String?,
     @JsonProperty("backdrop_path") val backdropPath: String?,
     @JsonProperty("genres") val genres: List<Genre>?,
-    @JsonProperty("overview") val overview: String,
-    @JsonProperty("vote_average") val rating: Float,
-    @JsonProperty("release_date") val openDate: Date
+    @JsonProperty("overview") val overview: String?,
+    @JsonProperty("vote_average") val rating: Float?,
+    @JsonProperty("release_date") val openDate: Date?
 )
 
 data class MovieSearchResult(
