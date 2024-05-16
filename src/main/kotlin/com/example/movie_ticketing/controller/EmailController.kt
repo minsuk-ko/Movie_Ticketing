cc08bb8bbefb234ff95277cea8c16e3b496a1b61
nek0 <juhyeonbt861@naver.com>
2024-05-17 00:52:35 +0900

    email 인증 완료 + 주석 추가

--- src/main/kotlin/com/example/movie_ticketing/controller/EmailController.kt
+++ src/main/kotlin/com/example/movie_ticketing/controller/EmailController.kt
@@ -22,7 +22,12 @@ class EmailController(private val emailService: EmailService) {
 
     @PostMapping("/verify-code")
     fun verifyCode(@RequestBody request: VerifyCodeRequest): ResponseEntity<Map<String, Boolean>> {
+        // 요청 본문(Body)에서 전달된 이메일 주소와 코드를 사용하여,
+        // emailService의 verifyCode 메소드를 호출하여 코드의 유효성을 검사
         val isValid = emailService.verifyCode(request.email, request.code)
+
+        // 검사 결과를 Map 객체에 담아 ResponseEntity 객체를 통해 클라이언트에게 JSON 형태로 응답
+        // 이 때, "valid"라는 키를 사용하여 유효성 검사 결과를 전달함
         return ResponseEntity.ok(mapOf("valid" to isValid))
     }
 }
