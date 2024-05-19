5f46d18b8bcdf3a10844284b5c2b71f00345f663
minsuk <rhalstjr106@gmail.com>
2024-05-19 16:50:50 +0900

    일단 둘다표시되는버전

--- src/main/kotlin/com/example/movie_ticketing/SecurityConfig.kt
+++ src/main/kotlin/com/example/movie_ticketing/SecurityConfig.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.UserDetailsService
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
@@ -41,4 +42,5 @@ class SecurityConfig {
         }
         return http.build()
     }
+
 }
